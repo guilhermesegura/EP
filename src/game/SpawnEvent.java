9cf27ca778c112b2169c18de4b87ceeb4ce7ef8b
HigorRanel <higorranel@gmail.com>
2025-06-16 17:38:40 -0300

    Levels, loader, spawn e arquivos de config

---
+++ src/game/SpawnEvent.java
@@ -0,0 +1,35 @@
+package game;
+
+public class SpawnEvent {
+  public enum Type {INIMIGO, CHEFE}
+
+  private Type type;
+  private int entityType;
+  private int time;
+  private int x;
+  private int y;
+  private int life; // será aplicado apenas para os boss
+
+  public SpawnEvent(Type type, int entityType, int time, int x, int y, int life){
+        this.type = type;
+        this.entityType = entityType;
+        this.time = time;
+        this.x = x;
+        this.y = y;
+        this.life = life;
+  }
+    
+
+    public Type getType() { 
+        return type; }
+    public int getEntityType() { 
+        return entityType; }
+    public int getTime() { 
+        return time; }
+    public int getX() { 
+        return x; }
+    public int getY() { 
+        return y; }
+    public int getLife() { 
+        return life; }
+}
